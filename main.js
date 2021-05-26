b6aabc13065c52acd07d89e62c48b8c7b6529ca2
chiya12345 <78139253+chiya12345@users.noreply.github.com>
2021-05-26 16:42:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+canvas =
+    document.getElementById("mycanvas");
+ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 1;
+//ctx.rect(150, 143, 430, 200);
+ctx.arc(250,210,40,0,2 * Math.PI);
+ctx.stroke();
